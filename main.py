26cdee0f841a4c49577b656e3641945dada9d81b
Jusinka <shaurma123123123@gmail.com>
2023-06-25 01:11:46 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# import this "как я понимаю написанное что одно лучше другого "
+# import antigravity "Python может научить летать   "
+# import __hello__  просто print  'Hello World'
+# from __future__ import braces "Не запустилось "
+
+a = '*'
+b =  ' '
+print(a+b+a+b+b+b+b+b+b+b+a+b+a+b+a+b+b+b+b+b+a+b+a+b+b+b+b+b+b+b+b+a)
+a = '*'
+b = ' '
+print(a+b+b+b+a+b+b+b+a+b+b+b+b+b+b+b+a+b+b+b+a+b+b+b+a+b+b+b+b+b+a+b+a)
+a = '*'
+b = ' '
+print(a+a+a+b+b+b+b+b+a+b+b+b+b+b+b+b+a+b+b+b+a+a+a+b+b+b+b+b+b+a+a+a+a+a)
+a = '*'
+b = ' '
+print(a+b+b+b+a+b+b+b+a+b+b+b+b+b+b+b+a+b+b+b+a+b+b+b+a+b+b+b+a+b+b+b+b+b+a)
+a = '*'
+b = ' '
+print(a+b+a+b+b+b+b+b+b+b+a+b+a+b+a+b+b+b+b+b+a+b+a+b+b+b+b+a+b+b+b+b+b+b+b+a)
+print('Escape sequences')
+print(' \a Bell (alert)')
+print('\b Backspace')
+print('\n New line')
+print('Horizontal tab')
+print('\\ Backslash \ ')
+print('\"Double quotation mark"')
+print('\' Single quotation mark ')
+x = 9
+y = 8
+print("x = {}, y = {}".format(x, y))
+a = 1
+y = 5
+print(a+y)
+a = 4
+y = 12
+print(y // a)
+a = 32
+b = 2
+print(a * b)
+a = 16
+b = 4
+print(a-b)
+
